34e8dcfd6f86a0e495c26085b8dc641b8f4b2f60
Matthew Webb <matt.d.webb@icloud.com>
2019-03-27 01:47:18 +0000

    added google tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,7 +100,7 @@ module.exports = {
   {
     resolve: `gatsby-plugin-google-analytics`,
     options: {
-      trackingId: 'UA-XXXX-Y',
+      trackingId: 'UA-137019501-1',
       // Puts tracking script in the head instead of the body
       head: true,
       // IP anonymization for GDPR compliance

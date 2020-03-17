0c7567daad2aa005ca842708e0d924a54052bd54
Matthew Webb <matt.d.webb@icloud.com>
2020-03-17 20:39:43 +0000

    remove chess files

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,8 +4,6 @@ import SiteNav from '../components/header/SiteNav';
 import { SiteHeader, outer, inner, SiteMain } from '../styles/shared';
 import * as React from 'react';
 import { css } from '@emotion/core';
-import Chessboard from 'chessboardjsx';
-import Stockfish from '../components/Stockfish';
 import { PostFullHeader, PostFullTitle, NoImage, PostFull } from '../templates/post';
 import { PostFullContent } from '../components/PostContent';
 import Footer from '../components/Footer';
@@ -17,16 +15,6 @@ const PageTemplate = css`
     padding-bottom: 4vw;
   }
 `;
-
-const boardsContainer = {
-  display: "flex",
-  justifyContent: "space-around",
-  alignItems: "center"
-};
-const boardStyle = {
-  borderRadius: "5px",
-  boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
-};
 
 const About: React.FunctionComponent = () => (
   <IndexLayout>
@@ -55,24 +43,6 @@ const About: React.FunctionComponent = () => (
               <p>
                 Try playing my favourite game, Chess:
               </p>
-              <div style={boardsContainer}>
-                {typeof window !== 'undefined' ?
-                  <Stockfish>
-                    {({ position, onDrop }: any) => (
-                      <Chessboard
-                        id="stockfish"
-                        position={position}
-                        width={320}
-                        onDrop={onDrop}
-                        boardStyle={boardStyle}
-                        orientation="black"
-                      />
-                    )}
-                  </Stockfish> : null
-                }
-
-              </div>
-
             </div>
           </PostFullContent>
         </article>
